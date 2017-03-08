8ad6e2909f6536fb12bf2b9283d1f99b1b05a95c
ManuelBorja <mborja00@hotmail.com>
2017-03-08 11:10:12 -0500

    Carga Inicial

---
+++ platforms/android/assets/www/js/main.js
@@ -0,0 +1,21 @@
+
+// $(document).ready(function() {
+// 	$('#menu-des').on('click',function(){
+// 	  $('#main-nav').toggleClass('mostrar')
+// 	  $('#menu-des').toggleClass('mover')
+// 	})
+// 	$('#setting').on('click', function(){
+// 		$('#lenguages').toggleClass('mostrar')
+// 	})
+// }
+
+
+$(document).ready(function() {
+	$('.fa-bars').on('click',function(){
+	  $('.perfil2').toggleClass('mostrar');
+	})
+	$('#setting').on('click', function(){
+		$('#lenguages').toggleClass('mostrar');
+	})
+
+}
